606769d758850e1e783600a5e6a584389f632bc7
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2017-05-29 17:17:42 +0300

    Added settings.

--- src/src/app/app.component.spec.ts
+++ src/src/app/app.component.spec.ts
@@ -21,14 +21,14 @@ describe(`App`, () => {
    */
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
+      declarations: [AppComponent],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [AppState]
     })
-    /**
-     * Compile template and css
-     */
-    .compileComponents();
+      /**
+       * Compile template and css
+       */
+      .compileComponents();
   }));
 
   /**
@@ -36,7 +36,7 @@ describe(`App`, () => {
    */
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
-    comp    = fixture.componentInstance;
+    comp = fixture.componentInstance;
 
     /**
      * Trigger initial data binding
