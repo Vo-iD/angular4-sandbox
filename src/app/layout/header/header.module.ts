392baed1241fd1cbb67e02b48e457fc1f90726b7
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2017-06-12 19:50:25 +0300

    [Task 2] Added simple header.

--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { LogoComponent } from './logo/logo.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
+  imports: [
+    RouterModule
+  ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
   ],
   declarations: [
     LogoComponent,
