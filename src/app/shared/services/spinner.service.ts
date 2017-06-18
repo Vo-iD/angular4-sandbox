12ee0be54cf67365764e7834d221375a57a19a85
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2017-06-18 20:03:30 +0300

    [Task 6] Used Subject for spinner.

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { NgBlockUI, BlockUI } from 'ng-block-ui';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class SpinnerService {
+  private _block: Subject<boolean> = new Subject();
   @BlockUI() private _blockUI: NgBlockUI;
 
+  constructor() {
+    this._block.subscribe((blockRequested) => {
+      if (blockRequested) {
+        this._blockUI.start('Loading...');
+      } else {
+        this._blockUI.stop();
+      }
+    });
+  }
+
   public show(): void {
-    this._blockUI.start('Loading...');
+    this._block.next(true);
   }
 
   public hide(): void {
-    this._blockUI.stop();
+    this._block.next(false);
   }
 }
